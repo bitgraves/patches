b63993327ba06781dbd272c1091045cbbe0a2893
Ben Roth <spongebucket@gmail.com>
2023-09-11 16:23:57 -0700

    try adding test scene as property on window

--- show/sep2023/test.js
+++ show/sep2023/test.js
@@ -1,1 +1,8 @@
-export const testScene = () => solid(0,0,1);
+const exposeToWindow = (obj) =>
+      Object.entries(obj).forEach(([key, value]) => (window[key] = value));
+
+test = {
+  testScene: () => solid(0,0,1),
+};
+
+exposeToWindow(test);
